414030d01c7e7a54604f005c7a0a3b6b63795122
christianSeong <christian199@naver.com>
2021-04-10 02:43:10 +0900

    commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+"/dist/"));
+ app.use(express.static(__dirname+"/dist/"));
 
 app.get(/.*/, function(req,res){
     res.sendFile(__dirname+"/dist/index.html");
