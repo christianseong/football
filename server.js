9cee931fae27a1fe01746e848bcdebf93d4259f7
christianSeong <christian199@naver.com>
2021-04-10 02:28:06 +0900

    git

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const app = express();
 
 app.use(express.static(__dirname+"/dist/"));
 
-app.get("/", function(req,res){
+app.get(/.*/, function(req,res){
     res.sendFile(__dirname+"/dist/index.html");
+    res.send("Hello")
 
 });
 
